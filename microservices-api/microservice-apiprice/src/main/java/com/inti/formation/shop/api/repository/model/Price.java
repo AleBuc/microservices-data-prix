0442d44c6bcd50a90669064a758f742ce11b44d5
unknown <vianneycharpentier@gmail.com>
2020-04-21 16:59:09 +0200

    Modele Price

---
+++ microservices-api/microservice-apiprice/src/main/java/com/inti/formation/shop/api/repository/model/Price.java
@@ -0,0 +1,38 @@
+package com.inti.formation.shop.api.repository.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import java.io.Serializable;
+
+
+/**
+ * @author Vianney
+ */
+
+@Data
+@Document(collection = "price")
+@CompoundIndexes({
+        @CompoundIndex(name = "price", def = "{ active: 1, date: 1 }", unique = false)
+        // unique = false acceptation des doublons true non
+        // unique = true rejet  des doublons
+})
+
+public class Price implements Serializable {
+    /**
+     * price identifier
+     */
+    @Id
+    private long idPrix;
+    @Indexed(unique = false)
+    private float montant;
+    private boolean active;
+    private String code;
+    private String date;
+
+}
