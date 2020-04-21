5c7bb4b3ad1f58cb5f881425aa4c90b90f17f664
AlexandreBuc <koneochester@gmail.com>
2020-04-21 15:32:15 +0200

    Mise en forme du projet

--- microservices-api/spring-reactive-mongoapi-shop/src/main/java/com/inti/formation/shop/api/rest/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.inti.formation.shop.api.rest.exception;
-
-/**
- * @author Sylvanius Kouandongui
- */
-public class BadRequestException extends Throwable {
-}
--- microservices-api/spring-reactive-mongoapi-shop/src/main/java/com/inti/formation/shop/api/rest/exception/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package com.inti.formation.shop.api.rest.exception;
-
-/**
- * @author Sylvanius Kouandongui
- */
-public class InternalServerException extends Throwable {
-}
