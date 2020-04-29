8926c233df3c78e6fcc8d96719dc15d825343b8d
CestVianney <vianneycharpentier@gmail.com>
2020-04-29 16:39:12 +0200

    Classes KafkaConsumerConfigurationnsumerConfiguration + Price completées

--- microservice-api-consumer/microservice-apiprice-consumer/src/main/java/com/inti/formation/apiconsumer/message/Price.java
+++ microservice-api-consumer/microservice-apiprice-consumer/src/main/java/com/inti/formation/apiconsumer/message/Price.java
@@ -1,4 +1,13 @@
 package com.inti.formation.apiconsumer.message;
 
+import java.util.Date;
+
 public class Price {
+
+    private long idPrix;
+    private float montant;
+    private boolean active;
+    private String code;
+    private Date date;
+
 }
