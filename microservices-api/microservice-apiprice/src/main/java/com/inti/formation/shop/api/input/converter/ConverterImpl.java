3d7d898b2a2583ceaee86f06a3f82ec7f5752769
CestVianney <vianneycharpentier@gmail.com>
2020-05-06 17:13:49 +0200

    Remplissage converters/impl ; PriceInput

--- microservices-api/microservice-apiprice/src/main/java/com/inti/formation/shop/api/input/converter/ConverterImpl.java
+++ microservices-api/microservice-apiprice/src/main/java/com/inti/formation/shop/api/input/converter/ConverterImpl.java
@@ -1,4 +1,26 @@
 package com.inti.formation.shop.api.input.converter;
 
-public class ConverterImpl {
+import com.inti.formation.shop.api.input.model.PriceInput;
+import com.inti.formation.shop.api.repository.model.Price;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class ConverterImpl implements Converter {
+
+
+    @Override
+    public Price convert(PriceInput p) {
+
+        Price price = new Price();
+        price.setIdPrix(p.getIdPrix());
+        price.setMontant(p.getMontant());
+        price.setActive(p.isActive());
+        price.setCode(p.getCode());
+        price.setDate(p.getDate());
+        price.setDateSuppr(p.getDateSuppr());
+
+        return price;
+    }
 }
